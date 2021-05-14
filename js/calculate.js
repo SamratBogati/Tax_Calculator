0ba4b43fd69582e32a9425dccfb2cce515ed37de
SamratBogati <71712793+SamratBogati@users.noreply.github.com>
2021-05-14 14:04:45 +0545

    Logic for tax calculation of Nepal

--- js/calculate.js
+++ js/calculate.js
@@ -1,37 +1,76 @@
-// calculate tax pop up
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
-
-openModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = document.querySelector(button.dataset.modalTarget)
-    openModal(modal)
-  })
-})
-
-overlay.addEventListener('click', () => {
-  const modals = document.querySelectorAll('.modal.active')
-  modals.forEach(modal => {
-    closeModal(modal)
-  })
-})
-
-closeModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = button.closest('.modal')
-    closeModal(modal)
-  })
-})
-
-function openModal(modal) {
-  if (modal == null) return
-  modal.classList.add('active')
-  overlay.classList.add('active')
+function calculateTax(event){
+  event.preventDefault();
+  var status = document.getElementById("status").value;
+  var monthlySalary = document.getElementById("monthly-salary").value;
+  var months = document.getElementById("months-id").value;
+  var monthlyBonus = document.getElementById("bonus").value;
+  var providentFund = document.getElementById("provident-fund").value;
+  var trust = document.getElementById("trust").value;
+  var insurance = document.getElementById("insurance").value;
+  var income = monthlySalary + monthlyBonus - providentFund - trust - insurance;
+  console.log("Total Income: " + income);
+  if (status == "Married") {
+      if(months == "yearly"){
+        income = 12 *(monthlySalary + monthlyBonus - providentFund - trust - insurance);
+        if (income > 2000000) {
+          tax = 444000 + ((income - 2000000) * 36) / 100;
+        } else if (income <= 2000000 && income > 700000) {
+          tax = 54000 + ((income - 700000) * 30) / 100;
+        } else if (income <= 700000 && income > 500000) {
+          tax = 14000 + ((income - 500000) * 20) / 100;
+        } else if (income <= 500000 && income > 400000) {
+          tax = 4000 + ((income - 400000) * 10) / 100;
+        } else if (income <= 400000 && income > 0) {
+          tax = (income * 1) / 100;
+        }
+      }else if(months == "monthly"){
+        income = 12 *(monthlySalary + monthlyBonus - providentFund - trust - insurance);
+        if (income > 2000000) {
+          tax = 444000 + ((income - 2000000) * 36) / 100;
+        } else if (income <= 2000000 && income > 700000) {
+          tax = 54000 + ((income - 700000) * 30) / 100;
+        } else if (income <= 700000 && income > 500000) {
+          tax = 14000 + ((income - 500000) * 20) / 100;
+        } else if (income <= 500000 && income > 400000) {
+          tax = 4000 + ((income - 400000) * 10) / 100;
+        } else if (income <= 400000 && income > 0) {
+          tax = (income * 1) / 100;
+        }
+        monthlyTax = tax / 12;
+      }
+      } else{
+        if(months == "yearly"){
+          income = 12 *(monthlySalary + monthlyBonus - providentFund - trust - insurance);
+          if (income > 2000000) {
+            tax = 429500 + ((income - 2000000) * 36) / 100;
+          } else if (income <= 2000000 && income > 750000) {
+            tax = 54500 + ((income - 750000) * 30) / 100;
+          } else if (income <= 750000 && income > 550000) {
+            tax = 14500 + ((income - 550000) * 20) / 100;
+          } else if (income <= 550000 && income > 450000) {
+            tax = 4500 + ((income - 450000) * 10) / 100;
+          } else if (income <= 450000 && income > 0) {
+            tax = (income * 1) / 100;;
+          }
+        } else if(months == "monthly"){
+          income = 12 *(monthlySalary + monthlyBonus - providentFund - trust - insurance);
+          if (income > 2000000) {
+            tax = 429500 + ((income - 2000000) * 36) / 100;
+          } else if (income <= 2000000 && income > 750000) {
+            tax = 54500 + ((income - 750000) * 30) / 100;
+          } else if (income <= 750000 && income > 550000) {
+            tax = 14500 + ((income - 550000) * 20) / 100;
+          } else if (income <= 550000 && income > 450000) {
+            tax = 4500 + ((income - 450000) * 10) / 100;
+          } else if (income <= 450000 && income > 0) {
+            tax = (income * 1) / 100;
+          }
+          monthlyTax = tax / 12;
+        }
+      }
+      document.getElementById("yearly-tax").value = tax;
+      document.getElementById("monthly-tax").value = monthlyTax;
 }
 
-function closeModal(modal) {
-  if (modal == null) return
-  modal.classList.remove('active')
-  overlay.classList.remove('active')
-}
+var form = document.getElementById('tax_form');
+form.addEventListener('submit', calculateTax)
